a6cffbdd835caaac0f20d19c6def34eb6cba0a68
JoseLLopes <121820444+JoseLLopes@users.noreply.github.com>
2025-04-06 23:38:26 -0300

    feat: Add hit particle to projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,13 +6,21 @@ public class Projectile : MonoBehaviour
 {
     public int damage = 10; // Damage dealt by the projectile
     public ForceType forceType; // Type of force applied to the projectile
+    [SerializeField] GameObject explosionHitParticle;
+
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.CompareTag("Terrain"))
+        {
+            Instantiate(explosionHitParticle, transform.position, Quaternion.identity);
+        }
         if(other.TryGetComponent<IDamageable>(out IDamageable damageable))
         {
             damageable.TakeDamage(damage, forceType);
         }
+
+        Destroy(gameObject);
     }
 }
 
